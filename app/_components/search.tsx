7216d6bc3e00adf8b5b080d5e40aedb4a1eeefa9
Julian Benegas <julianbenegas99@gmail.com>
2024-05-22 19:15:35 -0400

    impl recent searches

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,6 +5,12 @@ export const Search = ({ _searchKey }: { _searchKey: string }) => {
   const search = useSearch({
     _searchKey,
     queryBy: ["_title", "body", "excerpt"],
+    saveRecentSearches: {
+      getStorage() {
+        return localStorage;
+      },
+      key: "recent-posts-searches",
+    },
   });
 
   return (
@@ -20,7 +26,68 @@ export const Search = ({ _searchKey }: { _searchKey: string }) => {
 
         <div className="mt-2 border boder-gray-200 p-2 rounded-lg h-[200px] overflow-auto">
           <SearchBox.Placeholder className="text-sm">
-            Start typing to search.
+            {search.recentSearches?.hits?.length ? (
+              <SearchBox.HitList className="space-y-1">
+                <h3>Recent Searches</h3>
+                {search.recentSearches.hits.map((hit) => {
+                  return (
+                    <div className="relative" key={hit._key}>
+                      <SearchBox.HitItem
+                        hit={hit}
+                        href={`/blog/${hit.document._slug}`}
+                        className={`data-[selected="true"]:bg-gray-100 flex flex-col gap-1 p-1 rounded-md pr-20`}
+                      >
+                        <SearchBox.HitSnippet
+                          fieldPath="_title"
+                          components={{
+                            mark: (props) => (
+                              <span
+                                {...props}
+                                className="bg-red-200 rounded-sm"
+                              />
+                            ),
+                            container: (props) => (
+                              <div
+                                {...props}
+                                className="text-sm font-semibold overflow-hidden text-ellipsis whitespace-nowrap"
+                              />
+                            ),
+                          }}
+                        />
+                        <SearchBox.HitSnippet
+                          fieldPath="body"
+                          fallbackFieldPaths={["excerpt"]}
+                          components={{
+                            mark: (props) => (
+                              <mark
+                                {...props}
+                                className="bg-red-200 rounded-sm"
+                              />
+                            ),
+                            container: (props) => (
+                              <div
+                                {...props}
+                                className="text-xs line-clamp-2"
+                              />
+                            ),
+                          }}
+                        />
+                      </SearchBox.HitItem>
+                      <button
+                        className="absolute top-1/2 right-2 -translate-y-1/2 z-10 text-xs bg-red-200 text-red-800 px-1 rounded-sm"
+                        onClick={() => {
+                          search.recentSearches?.remove(hit._key);
+                        }}
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  );
+                })}
+              </SearchBox.HitList>
+            ) : (
+              <>Start typing to search.</>
+            )}
           </SearchBox.Placeholder>
 
           <SearchBox.Empty className="text-sm">
@@ -40,7 +107,7 @@ export const Search = ({ _searchKey }: { _searchKey: string }) => {
                     fieldPath="_title"
                     components={{
                       mark: (props) => (
-                        <mark {...props} className="bg-yellow-200 rounded-sm" />
+                        <mark {...props} className="bg-red-200 rounded-sm" />
                       ),
                       container: (props) => (
                         <div
@@ -55,7 +122,7 @@ export const Search = ({ _searchKey }: { _searchKey: string }) => {
                     fallbackFieldPaths={["excerpt"]}
                     components={{
                       mark: (props) => (
-                        <mark {...props} className="bg-yellow-200 rounded-sm" />
+                        <mark {...props} className="bg-red-200 rounded-sm" />
                       ),
                       container: (props) => (
                         <div {...props} className="text-xs line-clamp-2" />
